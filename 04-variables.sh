a99d90c27be1f38bdef4e9e143ebb11b26ebfc18
kousik25 <kousikdittakavi1@gmail.com>
2023-12-13 19:53:03 +0530

    2nd script

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,6 +2,10 @@
 
 echo "please enter your username"
 
+echo "please enter your password"
+
 read -s USERNAME # the value entered above will be automatically attached yo username
+
+read -s PASSWORD
 
 #echo "username is: $USERNAME, Password is: $PASSWORD"
