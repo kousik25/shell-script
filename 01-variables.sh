76e1af0ab8e144cd0cf9e0b92c93b9064707f1e3
kousik25 <kousikdittakavi1@gmail.com>
2023-12-13 18:33:41 +0530

    2nd script

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Ramesh:: hello suresh, Good Morning"
-echo "Suresh:: Hi Ramesh, VGM"
-echo "Ramesh:: H R U Suresh"
-echo "Suresh:: I am pretty good Ramesh, H R U"
+P1=Ramesh
+P2=Suresh
+echo "$P1:: hello $P2, Good Morning"
+echo "$P2:: Hi $P1, VGM"
+echo "$P1:: H R U $P2"
+echo "$P2:: I am pretty good $P1, H R U"
