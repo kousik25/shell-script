92e7547bfea7f340c64c33fcdcd8414ca0f763f2
kousik25 <kousikdittakavi1@gmail.com>
2023-12-18 18:40:58 +0530

    2nd script

--- counting-words.sh
+++ counting-words.sh
@@ -10,10 +10,10 @@
 file_path="/PracticeDevops/Repos/shell-script/demo.txt"
 
 # using wc command to count number of lines
-number_of_lines= `wc --lines << $file_path`
+number_of_lines= `wc --lines <<< $file_path`
 
 # using wc command to count number of words
-number_of_words= `wc --word << $file_path`
+number_of_words= `wc --word <<< $file_path`
 
 # Displaying number of lines and number of words
 echo "Number of lines: $number_of_lines"
