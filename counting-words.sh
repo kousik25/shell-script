f6d7053b7179f5ed5cd328a389b9b3e5a240ebe7
kousik25 <kousikdittakavi1@gmail.com>
2023-12-18 11:53:28 +0530

    2nd script

--- counting-words.sh
+++ counting-words.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-R="\e[31m"
-
 echo "Enter a word"
 text=$1
-word=$(echo $R "$text" | wc -w)
+word=$(echo -n "$text" | wc -w)
 echo "Number of Words = $word" ##Displays the no of words from text
